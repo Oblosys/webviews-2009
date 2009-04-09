a89a614bfe750830c44fe6537eb823b41704efc7
Oblosys <martijn@oblomov.com>
2009-04-09 18:59:10 +0000

    new name
    git-svn-id: svn://svn.oblomov.com/Oblomov@13 63c81c92-1a08-11de-87e6-23293a004a03

--- src/Database.hs
+++ src/Database.hs
@@ -4,12 +4,14 @@ data Root = Root Vet deriving Show
 
 data Vet = Vet { name :: String, visits :: [Visit]} deriving Show
 
+--data Visit = Visit { zipCode :: String, date :: String, sties :: [Sty] } deriving Show
 data Visit = Visit { zipCode :: String, date :: String, sties :: [Sty] } deriving Show
 
 data Sty = Sty { pigs :: [Pig] } deriving Show
 
 data Pig = Pig { symptoms :: [Int], diagnose :: Either Int String } deriving Show
 
+{-
 root :: Root
 root = Root $ Vet "Martijn"
                 [ Visit "3581" "27-3-2009" $
@@ -21,3 +23,4 @@ root = Root $ Vet "Martijn"
                     [ Sty [ Pig [0,0,0] (Left 2) ]
                     ]
                 ]
+-}
