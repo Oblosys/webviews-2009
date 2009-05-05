ee49a0fcfe421e05f077890387d800528cbcfdbe
Oblosys <martijn@oblomov.com>
2009-05-05 15:50:28 +0000

    better table, some color functions, some size stuff
    git-svn-id: svn://svn.oblomov.com/Oblomov@138 63c81c92-1a08-11de-87e6-23293a004a03

--- src/HtmlLib.hs
+++ src/HtmlLib.hs
@@ -18,6 +18,8 @@ mkDiv str elt = thediv![identifier str] << elt
 
 mkSpan str elt = thespan![identifier str] << elt
 
+spaces i = primHtml $ concat $ replicate i "&nbsp;"
+
 boxed html = thediv![thestyle "border:solid; border-width:1px; padding:4px;"] << html
 
 hList [] = noHtml
@@ -26,20 +28,41 @@ hList views = simpleTable [] [] [ views ]
 vList [] = noHtml
 vList views = simpleTable [] [] [ [v] | v <- views ]
 
+mkTable :: [HtmlAttr] -> [[HtmlAttr]] -> [[Html]] -> Html
+mkTable tableAttrs rowAttrss rows =
+  table!tableAttrs << concatHtml
+    [ tr!rowAttrs << map td row 
+    | (rowAttrs, row) <- zip (rowAttrss++repeat []) rows
+    ] -- if no row attrss are given (or not enough), just assume no attrs ([])
+
 data Color = Rgb Int Int Int
            | Color String deriving Show
+
+withColor color elt = thediv ! [colorAttr color] << elt
+
+htmlColor :: Color -> String
+htmlColor (Rgb r g b) = "#" ++ toHex2 r ++ toHex2 g ++ toHex2 b
+htmlColor (Color colorStr) = colorStr
+
+
+-- style attribute declarations cannot be combined :-( so setting color and then size as attrs
+-- will fail. But as Html->Html with divs, there is no problem
+fgbgColorAttr color bgColor = 
+  thestyle $ "color: "++htmlColor color++"; background-color: "++htmlColor bgColor++";"
+
+colorAttr color = 
+  thestyle $ "color: "++htmlColor color++";"
 
 withBgColor color elt = thediv ! [bgColorAttr color] << elt
 
 bgColorAttr color = 
-  let colorStr = case color of
-                     Rgb r g b      -> "#" ++ toHex2 r ++ toHex2 g ++ toHex2 b
-                     Color colorStr -> colorStr
-  in thestyle $ "background-color: "++colorStr++";"
+  thestyle $ "background-color: "++htmlColor color++";"
 
 withSize width height elt = thediv! [thestyle $ "width: "++show width++"px;" ++
                                                 "height: "++show height++"px;" ++
                                                 "overflow: auto" ] << elt
+withHeight height elt = thediv! [thestyle $ "height: "++show height++"px;" ++
+                                            "overflow: auto" ] << elt
 
 constrain mn mx x = (mn `max` x) `min` mx
 
