5d9dfb0fdd7be818bc99a99120f6216624e684e9
Oblosys <martijn@oblomov.com>
2011-02-26 16:25:07 +0000

    todo's
    git-svn-id: svn://svn.oblomov.com/Oblomov@1848 63c81c92-1a08-11de-87e6-23293a004a03

--- src/Main.hs
+++ src/Main.hs
@@ -8,3 +8,8 @@ import Generics -- for testing in ghci
 
 main :: IO ()
 main = server                                                                
+
+{-
+Check lenses
+Check relation to google toolkit. Maybe we can use stuff from it?
+-}
