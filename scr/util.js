e0dfd78052a6a31cbcd89e7b0c9336b36b209401
Oblosys <martijn@oblomov.com>
2011-10-05 12:25:35 +0000

    spinners are cleared on server response
    git-svn-id: svn://svn.oblomov.com/Oblomov@2663 63c81c92-1a08-11de-87e6-23293a004a03

--- scr/util.js
+++ scr/util.js
@@ -1,13 +1,14 @@
 // make sure that jQuery is loaded before this file is loaded
+var spinners = new Array();
 
 function addSpinner(id) {    
   var opts = {
     lines: 12, // The number of lines to draw
     length: 7, // The length of each line
-    width: 4, // The line thickness
-    radius: 10, // The radius of the inner circle
+    width: 3, // The line thickness
+    radius: 7, // The radius of the inner circle
     color: '#000', // #rgb or #rrggbb
-    speed: 1, // Rounds per second
+    speed: 0.5, // Rounds per second
     trail: 60, // Afterglow percentage
     shadow: false // Whether to render a shadow
   };
@@ -19,5 +20,16 @@ function addSpinner(id) {
   console.log(id, targetElt, $targetElt.height());
   var divWidth = $targetElt.width();
   var divHeight = $targetElt.height();
-  $targetElt.prepend('<div style="z-index: 100; position:absolute; width:'+divWidth+'px; height:'+divHeight+'px; background-color:grey; opacity:0.4; filter:alpha(opacity=40); /* For IE8 and earlier */"></div>');
+  var overlay = $('<div style="z-index: 100; position:absolute; width:'+divWidth+'px; height:'+divHeight+'px; background-color:grey; opacity:0.1; filter:alpha(opacity=10); /* For IE8 and earlier */"></div>');
+  $targetElt.prepend(overlay);
+  console.log('Spinner '+spinner);
+  spinners.push({spinner: spinner, overlay: overlay});
+}
+
+function clearSpinners() {
+	for (i=0; i<spinners.length; i++) {
+		console.log('Spinner '+spinners[i]);
+		spinners[i].spinner.stop();
+		spinners[i].overlay.remove();
+	}
 }
