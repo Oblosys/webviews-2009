ae7152514afd609d92c77e1a0533aea63d7f941d
Oblosys <martijn@oblomov.com>
2011-10-16 14:16:56 +0000

    hiding everything wrt phantom-type safety
    git-svn-id: svn://svn.oblomov.com/Oblomov@2722 63c81c92-1a08-11de-87e6-23293a004a03

--- src/Reservations/ClientWebView.hs
+++ src/Reservations/ClientWebView.hs
@@ -14,9 +14,9 @@ import qualified Data.IntMap as IntMap
 import Debug.Trace
 import System.Time
 import Data.Time.Calendar
-import Types
+import Types hiding (WebView, ViewId, getViewId)
 import Generics
-import WebViewPrim
+import WebViewPrim hiding (viewEdit, mkWebView)
 import WebViewLib
 import HtmlLib
 import Control.Monad.State
@@ -169,7 +169,7 @@ mkClientView = mkWebViewT $
                                                       , jsCallFunction vid "disenable" [] ] 
                   , jsFunction vid "disenable" [] [ "console.log(\"disenable: \","++jsVar vid "selectedNr"++","++jsVar vid "selectedDate"++","++jsVar vid "selectedTime" ++" )"
                                                   , "var availables = "++jsVar vid "selectedDate"++" == null ? null : availability["++jsVar vid "selectedDate"++"].availables"
-                                                  , "var buttonIds = [\""++intercalate "\",\"" (map (show . getViewId) $ concat timeButtonss)++"\"]"
+                                                  , "var buttonIds = [\""++intercalate "\",\"" (map (show . getViewIdT_) $ concat timeButtonss)++"\"]"
                                                   , jsFor "var i=0;i<buttonIds.length;i++" $ 
                                                       [ "document.getElementById(buttonIds[i]).disabled = availables == null ? true : availables[i]<"++jsVar vid "selectedNr"
                                                       ]
