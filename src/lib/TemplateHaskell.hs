02488db2dbe7b6dd393418c539fe5c15900217fb
Oblosys <martijn@oblomov.com>
2012-06-01 17:30:46 +0000

    let TH depend on Types again, as removing dependency does not prevent packages from being loaded during executable compilation
    git-svn-id: svn://svn.oblomov.com/Oblomov@2808 63c81c92-1a08-11de-87e6-23293a004a03

--- src/lib/TemplateHaskell.hs
+++ src/lib/TemplateHaskell.hs
@@ -4,25 +4,13 @@ module TemplateHaskell where
 import Language.Haskell.TH
 import Types
 
-{-
-
-The template Haskell code is separate from Types.hs. This causes a bit more verbose code, and postpones some
-errors to executable compile time rather than library compile time (mainly undefined-variable errors, as type errors
-are not detected at library compile time anyway), but it has as advantage that we don't need to run Types.hs during compilation
-(which causes a lot of packages to be imported, making compilation very verbose) 
-
--}
-
 -- template Haskell code for deriving Initial instances
 -- usage: deriveInitial ''DataType     (must appear after the declaration of the data type)
 -- Unfortunately, deriveInitial causes declarations to disappear, unless put at the end of the source file (probably a GHC 7.4.1 bug)
 
 deriveInitial :: Name -> Q [Dec]
-deriveInitial tName = 
- do { i <- instanceD (return []) (appT (conT $ mkName "Initial") (conT tName)) 
-             [ funD (mkName "initial") [ clause [] (normalB body) [] ] ]
-    ; return [i]
-    }
+deriveInitial tName = [d|instance Initial $(conT tName) where
+                           initial = $(body) |]
  where body = do { TyConI (DataD _ _ _ (firstCon:_) _) <- reify tName
                  ; let (cName, cArity) = conNameArity firstCon
                  ; b <- applyN cName cArity
@@ -36,6 +24,5 @@ deriveInitial tName =
 
        -- e.g. applyN Cons 3 = Cons initial initial initial 
        applyN name 0 = conE name
---       applyN name n = appE (applyN name $ n-1) [e| initial |]
-       applyN name n = appE (applyN name $ n-1) (varE $ mkName "initial")
+       applyN name n = appE (applyN name $ n-1) [e| initial |]
        
