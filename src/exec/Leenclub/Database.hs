6766e790b830261eb1afd86f61985c94402743bc
Oblosys <martijn@oblomov.com>
2012-06-19 20:30:22 +0000

    Basic support for borrowing an item
    git-svn-id: svn://svn.oblomov.com/Oblomov@2890 63c81c92-1a08-11de-87e6-23293a004a03

--- src/exec/Leenclub/Database.hs
+++ src/exec/Leenclub/Database.hs
@@ -58,7 +58,8 @@ newLender db =
           
 data Item = 
   Item { itemId :: ItemId, itemOwner :: LenderId, itemPrice :: Int, itemName :: String, itemDescr :: String
-      } deriving (Eq, Show, Read, Typeable,Data)
+       , itemBorrowed :: Maybe LenderId
+       } deriving (Eq, Show, Read, Typeable,Data)
 
 -- put id in element? It is also in the map.
 
@@ -82,7 +83,7 @@ newItem :: LenderId -> Database -> (Item, Database)
 newItem uid db =
   let ids = [ i | ItemId i <- map fst (Map.toList $ allItems db) ]
       newId = ItemId $ if null ids then 0 else (maximum ids + 1)
-      newItem = Item newId uid 1 "<new>" ""
+      newItem = Item newId uid 1 "<new>" "" Nothing
   in  ( newItem, db { allItems = Map.insert newId newItem (allItems db) } )
 
 
@@ -104,18 +105,25 @@ mkInitialDatabase =
 
 spullen = [ Item (ItemId 0) (LenderId "martijn") 2 "Oblomov"
                                                  "Een boek schrijven van ruim vijfhonderd bladzijden waarin de held bijna tweehonderd bladzijden lang zijn bed niet uit komt, maar dat geen moment verveelt, zoiets is alleen een zeer groot schrijver gegeven. Oblomov, het magnum opus van Ivan Gontsjarov, is een roman die alles mist waar zoveel andere boeken het van moeten hebben; we lezen slechts over de kleine, alledaagse belevenissen van de goedige, maar aartsluie Ilja Oblomov. Zelfs zijn liefde voor de betoverende Olga kan Oblomov niet uit zijn apathie halen en hem aanzetten tot de grootse daden die zij van hem verwacht. De ondergang van een antiheld."
+                                                 Nothing
           , Item (ItemId 1) (LenderId "martijn") 3 "Grand Theft Auto 4"
                                                  "Wat is er tegenwoordig nog over van die legendarische 'American Dream'? Niko Bellic is net aan wal gestapt na een lange bootreis uit Europa en hoopt in Amerika zijn verleden te begraven. Zijn neef Roman droomt ervan het helemaal te maken in Liberty City, in het land van de onbegrensde mogelijkheden.\nZe raken in de schulden en komen in het criminele circuit terecht door toedoen van oplichters, dieven en ander tuig. Langzamerhand komen ze erachter dat ze hun dromen niet kunnen waarmaken in een stad waar alles draait om geld en status. Heb je genoeg geld, dan staan alle deuren voor je open. Zonder een cent beland je in de goot."
+                                                 Nothing
           , Item (ItemId 2) (LenderId "martijn") 2 "iPhone 3gs"
                                                  "Apple iPhone 3gs, 32Gb"
+                                                 (Just $ LenderId "henny")
           , Item (ItemId 3) (LenderId "jaap")    1 "Boormachine"
                                                  "Het krachtige en compacte toestel Krachtig motor van 600 Watt, ideaal om te boren tot 10 mm boordiameter in metaal Bevestiging van boorspil in het lager voor hoge precisie Compact design en gering gewicht voor optimale bediening bij middelzware boortoepassingen Besturings-electronic voor exact aanboren Metalen snelspanboorhouder voor hoge precisie en lange levensduur Rechts- en linksdraaien Bijzonder geschikt voor boorgaten tot 10 mm in staal Functies: Rechts- en linksdraaien Electronic Softgrip Leveromvang: Snelspanboorhouder 1 - 10 mm"
+                                                 Nothing
           , Item (ItemId 4) (LenderId "jaap")    1 "Spyder calibratie-apparaat"
                                                  "De Datacolor Spyder 4 Elite geeft nauwkeurig en natuurgetrouwe kleuren bij fotobewerkingen, films en games. Daarmee is hij geschikt voor professionele fotografen en andere creatievelingen. Verder is dit de eerste Spyder die iPhone en iPad ready is. Dit betekent dat hij via een app kan kalibreren met deze gadgets en de weergave van kleuren op je smartphone of tablet kan optimaliseren."
+                                                 Nothing
           , Item (ItemId 5) (LenderId "henny")   2 "Tomtom"
                                                  "Voor de prijsbewuste bestuurder die toch graag in breedbeeld navigeert is er de TomTom XL Classic. Uitermate gemakkelijk in gebruik - plug de 12V autoadapter in en begin onbezorgd aan je reis. Het IQ Routes systeem zorgt op ieder moment van de dag voor de snelste route."
+                                                 (Just $ LenderId "jaap")
           , Item (ItemId 6) (LenderId "henny")   1 "Boormachine"
                                                  "De Makita Accuboormachine BDF343SHE Li-Ion 14,4V beschikt niet alleen over uitzonderlijke krachten, hij is ook nog eens bijzonder comfortabel. Hij heeft namelijk een ergonomisch ontwerp meegekregen van Makita. Zo weet u zeker dat u nooit meer last van uw polsen of ellebogen hebt na het klussen. Daarnaast zorgt het softgrip handvat er voor dat hij erg lekker in de hand ligt. En ook blijft liggen, want zelfs met bezwete handen hanteert u hem nog steeds moeiteloos. Dus wilt u een machine die kracht en gebruikscomfort combineert, dan is de Makita Accuboormachine BDF343SHE Li-Ion 14,4V de juiste keuze."
+                                                 Nothing
           ]
 
 unsafeLookup map key = 
