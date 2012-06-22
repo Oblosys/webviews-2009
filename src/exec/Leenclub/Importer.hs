6acbcfce54f6f256d91a0ee44147ab74a3b48485
Oblosys <martijn@oblomov.com>
2012-06-22 21:52:01 +0000

    initial commit
    git-svn-id: svn://svn.oblomov.com/Oblomov@2916 63c81c92-1a08-11de-87e6-23293a004a03

---
+++ src/exec/Leenclub/Importer.hs
@@ -0,0 +1,96 @@
+module Importer where
+
+import Text.ParserCombinators.Parsec
+import System.IO
+import Data.Maybe
+import Data.List
+
+{-
+TODO: price in excel, excel in separate files
+image in Item
+other builders
+-}
+
+import Database
+builders = [ {- (buildUser, "user.csv")
+           ,  -} (buildCD, "cd.csv") {-, (buildBook, "book.csv"), (buildDvd, "dvd.csv")
+           , (buildGame, "game.csv"), (buildTool, "tool.csv"), (buildGadget, "gadget.csv"), (buildMisc, "misc.csv") -} ]
+
+main =
+ do { itemStrs <- fmap concat $ sequence [ importCSVFile build filename | (build, filename) <- builders ]
+    ; writeFile "Imported.hs" $ makeModule itemStrs
+    ; putStrLn $ concat itemStrs
+    }
+    
+importCSVFile build filename =
+ do { ecsv <- parseCSVFromFile filename
+    ; case ecsv of
+        Left err  -> error $ show err
+        Right csv -> return $ map show $ catMaybes $ map build $ drop 3 csv
+    }
+  
+buildCD :: Record -> Maybe Item
+buildCD [afb,eigenaar,titel,artiest,jaartal,genre,staat,beschrijving] | eigenaar /= "" =
+  Just $ Item (ItemId $ -1) (LenderId eigenaar) 10 titel beschrijving staat (CD artiest (read jaartal) genre) Nothing
+buildCD fields = Nothing
+
+ 
+ {-
+ Category = Book { bookAuthor :: String, bookYear :: Int, bookLanguage :: String, bookGenre :: String, bookPages :: Int, bookISBN :: String}
+              | Game { gamePlatform :: String, gameYear :: Int, gameDeveloper :: String, gameGenre :: String }
+              | CD   { cdArtist :: String, cdYear :: Int, cdGenre :: String }
+              | DVD  { dvdMovieOrSeries :: MovieOrSeries, dvdDirector :: String, dvdLanguage :: String, dvdYear :: Int, dvdGenre :: String
+                     , dvdRunningTime :: Int, dvdIMDb :: String, dvdSeason :: Int, dvdNrOfEpisodes :: Int }
+              | Tool { toolBrand :: String, toolType :: String, toolYear :: Int }
+              | Electronics {}
+              | Misc {} deriving (Eq, Show, Read, Typeable,Data)
+
+data Item = 
+  Item { itemId :: ItemId, itemOwner :: LenderId, itemPrice :: Int, itemName :: String, itemDescr :: String, itemState :: String
+       , itemCategory :: Category
+       , itemBorrowed :: Maybe LenderId
+       } deriving (Eq, Show, Read, Typeable,Data)
+
+ -}
+ 
+makeModule dataDecls = "module Imported where\n\nimport Database\n\nitems = [ " ++
+                       intercalate "\n        , " dataDecls ++ "\n        ]"
+ 
+type CSV = [Record]
+
+-- | A record is a series of fields
+type Record = [Field]
+
+-- | A field is a string
+type Field = String
+
+-- | A Parsec parser for parsing CSV files
+csv :: Parser CSV
+csv = do x <- record `sepEndBy` many1 (oneOf "\n\r")
+         eof
+         return x
+
+record :: Parser Record
+record = do { field <- (quotedField <|> field)
+            ; return $ interpretNewlines field 
+            }  `sepBy` char ';'
+
+interpretNewlines "" = ""
+interpretNewlines ('\\':'n':rest) = '\n' : interpretNewlines rest
+interpretNewlines (c:rest)       = c : interpretNewlines rest
+
+field :: Parser Field
+field = many (noneOf ";\n\r\"")
+
+quotedField :: Parser Field
+quotedField = between (char '"') (char '"') $
+              many (noneOf "\"" <|> try (string "\"\"" >> return '"'))
+
+-- | Given a file name (used only for error messages) and a string to
+-- parse, run the parser.
+parseCSV :: FilePath -> String -> Either ParseError CSV
+parseCSV = parse csv
+
+-- | Given a file name, read from that file and run the parser
+parseCSVFromFile :: FilePath -> IO (Either ParseError CSV)
+parseCSVFromFile = parseFromFile csv
