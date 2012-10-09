b8d1259c8985206f677c4f54cafc7fb8802b335f
Oblosys <martijn@oblomov.com>
2012-10-09 12:11:38 +0000

    Instead of classes, use attributes Answer and AnswerProgress for answered/unanswered status.
    git-svn-id: svn://svn.oblomov.com/Oblomov@3150 63c81c92-1a08-11de-87e6-23293a004a03

--- scr/js/BlijVanIT.js
+++ scr/js/BlijVanIT.js
@@ -3,9 +3,9 @@ function initProgressMarkers() {
   $('.ProgressLine').remove();
   
   var $formPage = $('.FormPage');
-  var $progressMarkers = $('.Answer').map(function() {
+  var $progressMarkers = $('[Answer]').map(function() {
     var $progressMarker = $("<div class='ProgressMarker'/>");
-    $progressMarker.addClass( $(this).hasClass('Answered') ? 'Answered' : 'Unanswered');
+    $progressMarker.attr('AnswerProgress', $(this).attr('Answer'));
     $progressMarker.css('top', $(this).position().top);
     $formPage.append($progressMarker);
     return $progressMarker;
@@ -14,8 +14,8 @@ function initProgressMarkers() {
     var topMarkerY = $progressMarkers[i-1].position().top;
     var bottomMarkerY = $progressMarkers[i].position().top;
     var length = bottomMarkerY - topMarkerY;
-    var isConnected = $progressMarkers[i-1].hasClass('Answered') &&
-                      $progressMarkers[i].hasClass('Answered');
+    var isConnected = $progressMarkers[i-1].attr('ProgressAnswer') == 'Answered' &&
+                      $progressMarkers[i].attr('ProgressAnswer') == 'Answered';
     
     var $progressLine = $("<div class='ProgressLine'>|</div>");
     $progressLine.addClass( isConnected ? 'Connected' : 'Disconnected');
