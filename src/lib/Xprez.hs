74f8a29ea4ec76f13019b683f61598861fcf646d
Oblosys <martijn@oblomov.com>
2012-11-20 16:37:16 +0000

    Allow setting the style + no more setting attrs on table.
    git-svn-id: svn://svn.oblomov.com/Oblomov@3315 63c81c92-1a08-11de-87e6-23293a004a03

--- src/lib/Xprez.hs
+++ src/lib/Xprez.hs
@@ -9,9 +9,10 @@ import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.String
 import Text.Blaze.Internal
 import Data.String
+import Data.List
 import Text.Show.Functions
   
-data Attrs = Attrs { getHStretch :: Bool, getVStretch :: Bool }
+data Attrs = Attrs { getHStretch :: Bool, getVStretch :: Bool, getStyle :: String }
 
 data Xprez = Html Html | Row [Xprez] | Col [Xprez] | Table [[Xprez]] | With (Attrs -> Attrs) Xprez deriving Show
   
@@ -37,13 +38,15 @@ hStretch = With (\a->a{getHStretch=True})
 vStretch = With (\a->a{getVStretch=True})
 stretch  = With (\a->a{getHStretch=True, getVStretch=True})
 
+addStyle :: String -> Xprez -> Xprez
+addStyle stl x = With (\a->a{getStyle=stl ++";" ++ getStyle a}) x
 
 xp :: Xprez -> Html
-xp x = let (html, attrs) = renderX x in html
+xp x = let (html, attrs) = renderX x 
+       in  setAttrs Nothing Nothing (getHStretch attrs) (getVStretch attrs) (getStyle attrs) $ html
 
 {-
 TODO: 
-- Maybe leave stretching of tables to parent table, so hStretch/vStretch not necessary anymore
 - Maybe we can even use 0% for width/height of stretching elts? It seems to lead to even distribution in Firefox & Safari.
 - Test vertical stretching.
 
@@ -53,38 +56,42 @@ Stretching: take proportional amount of space. Non-stretching, take minimal spac
 maximum space? So "this text" is not broken into "this" and "text"
 
 -}
-renderX (Html h) = (h, Attrs False False)
+renderX (Html h) = (h, Attrs False False "")
 renderX (Row xs) = let (hs, attrss) = unzip $ map renderX xs
+                       styles = map getStyle attrss
                        hStretches = map getHStretch attrss
                        hStretch = or hStretches
                        childWidth = 100 / (fromIntegral $ length $ filter id hStretches)
                        vStretches = map getVStretch attrss
                        vStretch = and vStretches
-                   in  ( htmlStretch Nothing Nothing hStretch vStretch $
-                           H.table ! A.class_ "Xprez" $
-                             H.tr $ concatHtml $ [ htmlStretch (Just childWidth) Nothing hStr vStr $ H.td $ htmlStretch Nothing Nothing hStr  vStr $ H.div $ h 
-                                                 | (h,hStr, vStr) <- zip3 hs hStretches vStretches 
-                                                 ] 
-                       , Attrs { getHStretch = hStretch, getVStretch = vStretch }
+                   in  ( H.table ! A.class_ "Xprez" $
+                           H.tr $ concatHtml $ [ setAttrs (Just childWidth) Nothing hStr vStr "" $ H.td $ 
+                                                   setAttrs Nothing Nothing hStr vStr stl $ H.div $ h 
+                                               | (h,hStr, vStr, stl) <- zip4 hs hStretches vStretches styles 
+                                               ] 
+                       , Attrs { getHStretch = hStretch, getVStretch = vStretch, getStyle = "" }
                        )
 renderX (Col xs) = let (hs, attrss) = unzip $ map renderX xs
+                       styles = map getStyle attrss
                        hStretches = map getHStretch attrss
                        hStretch = and hStretches
                        vStretches = map getVStretch attrss
                        vStretch = or vStretches
                        childHeight = 100 / (fromIntegral $ length $ filter id vStretches)
-                   in  ( htmlStretch Nothing Nothing hStretch vStretch $
-                           H.table ! A.class_ "Xprez" $ concatHtml $
-                             [ H.tr $ htmlStretch Nothing (Just childHeight) hStr vStr $ H.td $ htmlStretch Nothing Nothing hStr vStr $ H.div $ h 
-                             | (h,hStr, vStr) <- zip3 hs hStretches vStretches 
-                             ]                            
-                       , Attrs { getHStretch = hStretch, getVStretch = vStretch }
+                   in  ( H.table ! A.class_ "Xprez" $ concatHtml $
+                           [ H.tr $ setAttrs Nothing (Just childHeight) hStr vStr "" $ H.td $ 
+                                      setAttrs Nothing Nothing hStr vStr stl $ H.div $ h 
+                           | (h,hStr, vStr, stl) <- zip4 hs hStretches vStretches styles 
+                           ]                            
+                       , Attrs { getHStretch = hStretch, getVStretch = vStretch, getStyle = "" }
                        )
 renderX (With f x) = let (hs, attrs) = renderX x in (hs, f attrs)
 
-htmlStretch mWdth mHght hStr vStr x = x ! A.style ( toValue $ (if hStr then "width: "++widthStr mWdth++";" :: String else "") ++
-                                                              (if vStr then "height: "++heightStr mHght++";" else ""))
- where widthStr Nothing = "100%"
+setAttrs mWdth mHght hStr vStr stl h = if null styleStr then h else h ! A.style ( toValue styleStr)
+ where styleStr = (if hStr then "width: "++widthStr mWdth++";" :: String else "") ++
+                  (if vStr then "height: "++heightStr mHght++";" else "") ++
+                  stl
+       widthStr Nothing = "100%"
        widthStr (Just wdth) = showFloat 2 wdth ++ "%"
        heightStr Nothing = "100%"
        heightStr (Just hght) = showFloat 2 hght ++ "%"
