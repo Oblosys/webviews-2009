fd7175293d50b32d23755a934e4d60c100d9ef38
Oblosys <martijn@oblomov.com>
2012-11-20 20:29:59 +0000

    Added vertical alignment.
    git-svn-id: svn://svn.oblomov.com/Oblomov@3316 63c81c92-1a08-11de-87e6-23293a004a03

--- src/lib/Xprez.hs
+++ src/lib/Xprez.hs
@@ -11,8 +11,20 @@ import Text.Blaze.Internal
 import Data.String
 import Data.List
 import Text.Show.Functions
-  
-data Attrs = Attrs { getHStretch :: Bool, getVStretch :: Bool, getStyle :: String }
+
+
+data VAlign = Top | Middle | Bottom
+
+data Attrs = Attrs { getHStretch :: Bool, getVStretch :: Bool
+                   , getVAlign :: VAlign
+                   , getStyle :: String
+                   }
+{-
+Horizontal css align doesn't work like vertical align. It only seems to work on text, and in contrast to vertical align, we
+can set it on a div inside the table cell, so we don't need an attribute for it.
+-}
+defaultAttrs :: Attrs
+defaultAttrs = Attrs False False Middle ""
 
 data Xprez = Html Html | Row [Xprez] | Col [Xprez] | Table [[Xprez]] | With (Attrs -> Attrs) Xprez deriving Show
   
@@ -38,12 +50,15 @@ hStretch = With (\a->a{getHStretch=True})
 vStretch = With (\a->a{getVStretch=True})
 stretch  = With (\a->a{getHStretch=True, getVStretch=True})
 
+
+vAlign algn = With (\a->a{getVAlign=algn})
+
 addStyle :: String -> Xprez -> Xprez
 addStyle stl x = With (\a->a{getStyle=stl ++";" ++ getStyle a}) x
 
 xp :: Xprez -> Html
 xp x = let (html, attrs) = renderX x 
-       in  setAttrs Nothing Nothing (getHStretch attrs) (getVStretch attrs) (getStyle attrs) $ html
+       in  setChildAttrs (getHStretch attrs) (getVStretch attrs) (getStyle attrs) $ html
 
 {-
 TODO: 
@@ -51,51 +66,63 @@ TODO:
 - Test vertical stretching.
 
 
+-- use style "display: table-cell" to get rid of extra divs?
 
 Stretching: take proportional amount of space. Non-stretching, take minimal space. How to specify something not to stretch, but still take
 maximum space? So "this text" is not broken into "this" and "text"
 
 -}
-renderX (Html h) = (h, Attrs False False "")
+renderX (Html h) = (h, defaultAttrs)
 renderX (Row xs) = let (hs, attrss) = unzip $ map renderX xs
                        styles = map getStyle attrss
+                       vAlgns = map getVAlign attrss
                        hStretches = map getHStretch attrss
-                       hStretch = or hStretches
-                       childWidth = 100 / (fromIntegral $ length $ filter id hStretches)
                        vStretches = map getVStretch attrss
+                       hStretch = or hStretches
                        vStretch = and vStretches
+                       childWidth = 100 / (fromIntegral $ length $ filter id hStretches)
                    in  ( H.table ! A.class_ "Xprez" $
-                           H.tr $ concatHtml $ [ setAttrs (Just childWidth) Nothing hStr vStr "" $ H.td $ 
-                                                   setAttrs Nothing Nothing hStr vStr stl $ H.div $ h 
-                                               | (h,hStr, vStr, stl) <- zip4 hs hStretches vStretches styles 
+                           H.tr $ concatHtml $ [ setTDAttrs (Just childWidth) Nothing hStr vStr vAlgn $ H.td $ 
+                                                   setChildAttrs hStr vStr stl $ H.div $ h 
+                                               | (h,hStr, vStr, vAlgn, stl) <- zip5 hs hStretches vStretches vAlgns styles 
                                                ] 
-                       , Attrs { getHStretch = hStretch, getVStretch = vStretch, getStyle = "" }
+                       , defaultAttrs { getHStretch = hStretch, getVStretch = vStretch }
                        )
 renderX (Col xs) = let (hs, attrss) = unzip $ map renderX xs
                        styles = map getStyle attrss
+                       vAlgns = map getVAlign attrss
                        hStretches = map getHStretch attrss
-                       hStretch = and hStretches
                        vStretches = map getVStretch attrss
+                       hStretch = and hStretches
                        vStretch = or vStretches
                        childHeight = 100 / (fromIntegral $ length $ filter id vStretches)
                    in  ( H.table ! A.class_ "Xprez" $ concatHtml $
-                           [ H.tr $ setAttrs Nothing (Just childHeight) hStr vStr "" $ H.td $ 
-                                      setAttrs Nothing Nothing hStr vStr stl $ H.div $ h 
-                           | (h,hStr, vStr, stl) <- zip4 hs hStretches vStretches styles 
+                           [ H.tr $ setTDAttrs Nothing (Just childHeight) hStr vStr vAlgn $ H.td $ 
+                                      setChildAttrs hStr vStr stl $ H.div $ h 
+                           | (h,hStr, vStr, vAlgn, stl) <- zip5 hs hStretches vStretches vAlgns styles 
                            ]                            
-                       , Attrs { getHStretch = hStretch, getVStretch = vStretch, getStyle = "" }
+                       , defaultAttrs { getHStretch = hStretch, getVStretch = vStretch }
                        )
 renderX (With f x) = let (hs, attrs) = renderX x in (hs, f attrs)
 
-setAttrs mWdth mHght hStr vStr stl h = if null styleStr then h else h ! A.style ( toValue styleStr)
- where styleStr = (if hStr then "width: "++widthStr mWdth++";" :: String else "") ++
-                  (if vStr then "height: "++heightStr mHght++";" else "") ++
+
+setTDAttrs mWdth mHght hStr vStr vAlgn h = if null styleStr then h else h ! A.style ( toValue styleStr)
+ where styleStr = widthHeightStyle mWdth mHght hStr vStr ++
+                  case vAlgn of
+                    Top -> "vertical-align:top;"
+                    Middle -> ""
+                    Bottom -> "vertical-align:bottom;"
+ 
+setChildAttrs hStr vStr stl h = if null styleStr then h else h ! A.style ( toValue styleStr)
+ where styleStr = widthHeightStyle Nothing Nothing hStr vStr ++
                   stl
-       widthStr Nothing = "100%"
+       
+widthHeightStyle mWdth mHght hStr vStr = (if hStr then "width: "++widthStr mWdth++";" :: String else "") ++
+                                         (if vStr then "height: "++heightStr mHght++";" else "") 
+ where widthStr Nothing = "100%"
        widthStr (Just wdth) = showFloat 2 wdth ++ "%"
        heightStr Nothing = "100%"
        heightStr (Just hght) = showFloat 2 hght ++ "%"
-       
 
 row :: [Xprez]-> Xprez
 row xs = Row xs
