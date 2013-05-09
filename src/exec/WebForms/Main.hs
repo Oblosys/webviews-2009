2d0d79c9707eb4549d8fdb900a3fe71d86336966
Oblosys <martijn@oblomov.com>
2013-05-09 14:10:32 +0000

    Absolute path for image url
    git-svn-id: svn://svn.oblomov.com/Oblomov@3680 63c81c92-1a08-11de-87e6-23293a004a03

--- src/exec/WebForms/Main.hs
+++ src/exec/WebForms/Main.hs
@@ -653,7 +653,7 @@ mkMainView form = mkWebView $
      }
 
 instance Presentable MainView where
-  present (MainView fv) = mkPage [style "background: url('img/background_linen.png') repeat scroll center top transparent; min-height: 100%"] $
+  present (MainView fv) = mkPage [style "background: url('/img/background_linen.png') repeat scroll center top transparent; min-height: 100%"] $
                             with [class_ $ "MainPage"] $
                             present fv 
 
